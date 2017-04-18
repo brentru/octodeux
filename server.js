d786905d156e0c4d2d6669b6e3a02307622691e9
brentru <brent@xn.local>
2017-04-17 22:02:29 -0400

    push2

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 'use strict'
- 
+
+// requires
 const Hapi = require('hapi');
 const Request = require('request');
 const Vision = require('vision');
 const Handlebars = require('handlebars');
 const LodashFilter = require('lodash.filter');
 const LodashTake = require('lodash.take');
- 
+// create new hapi server
 const server = new Hapi.Server();
- 
 server.connection({
     host: '127.0.0.1',
     port: 3000
 });
  
-// Register vision for our views
+// view handler
 server.register(Vision, (err) => {
     server.views({
         engines: {
@@ -24,31 +24,38 @@ server.register(Vision, (err) => {
         path: './views',
     });
 });
+var request = require('request');
 
 // global info variable, WORK IN PROGRESS
 var info = 'infovar';
-var info2 = 'infovar2';
+var pinfo = 'PRINTERINFOVAR';
 
-var request = require('request');
+
+// API Key
 var apikey = '653288ED7823456E8F820AF3F15E2343';
+// API URL
+var apiURL = 'http://134.88.136.194:5000/api/'
 
-
+// Job Operations 
+// http://docs.octoprint.org/en/master/api/job.html
 var job = {
-  url: 'http://134.88.136.194:5000/api/job',
+  url: apiURL + 'job',
   headers: {
     'X-Api-Key': apikey
   }
 };
 
-var connection = {
-  url: 'http://134.88.136.194:5000/api/printer',
+// Printer Operations
+// TODO: test /printer
+// http://docs.octoprint.org/en/master/api/printer.html
+var printer = {
+  url: apiURL + 'printer/sd',
   headers: {
     'X-Api-Key': apikey
   }
 };
 
-
-
+// Job Callback + JSON Parse
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     info = JSON.parse(body);
@@ -57,13 +64,29 @@ function callback(error, response, body) {
   }
 }
 
+// Printer Callback + JSON Parse
+function callbackP(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    pinfo = JSON.parse(body);
+    // DEBUG 
+    console.log(pinfo);
+  }
+}
 
 
-
-//request(job, callback);
+// request printer status
+request(printer, callbackP);
+console.log(pinfo);
+// request job status
 request(job, callback);
 console.log(info);
 
+
+
+// START Happi Routes //
+// https://hapijs.com/tutorials/routing?lang=en_US
+
+// Main Dashboard
 server.route({
     method: 'GET',
     path: '/index',
@@ -73,9 +96,21 @@ server.route({
     }
 });
 
+// printer 1 status
+server.route({
+    method: 'GET',
+    path: '/p1op',
+    handler: function (request, reply) {
+        reply.view('p1op', 
+        { title: pinfo });
+    }
+});
 
 
 
+// END Happi Routes //
+
+// Server Info
 server.start((err) => {
     if (err) {
         throw err;
